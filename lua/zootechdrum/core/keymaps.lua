f3f760c48a90708e0ac416b0d60eda018b75b780
Cesar Gomez <cesargo@ultra.me>
2023-01-02 16:11:02 -0800

    add command disabled syntax highlighting

--- lua/zootechdrum/core/keymaps.lua
+++ lua/zootechdrum/core/keymaps.lua
@@ -35,6 +35,9 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+--disabled Treesitter highlight.
+keymap.set("n", "<leader>tsb", ":TSbufDisable highlight") --  go to previous tab
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
